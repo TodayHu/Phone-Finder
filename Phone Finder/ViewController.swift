f1e8dc01def9ddcc8e2cd45a39a9930773a01e0f
Michael Reiter <michaelreiter@live.ca>
2015-03-10 16:10:51 -0700

    Initial commit

---
+++ Phone Finder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Phone Finder
+//
+//  Created by Michael Reiter on 2015-03-10.
+//  Copyright (c) 2015 Michael Reiter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
