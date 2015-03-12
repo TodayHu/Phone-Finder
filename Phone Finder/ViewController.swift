3b8376cb2afcc72b482e42b90c0c871f9c256da7
Michael Reiter <michaelreiter@live.ca>
2015-03-11 21:50:25 -0700

    learning how to Swift

--- Phone Finder/ViewController.swift
+++ Phone Finder/ViewController.swift
@@ -12,14 +12,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let labelHeight: CGFloat = 100
+        let label = UILabel(frame: CGRectMake(0, 0, self.view.frame.size.width, labelHeight))
+        label.textAlignment = .Center
+        label.textColor = UIColor.blackColor()
+        label.backgroundColor = UIColor.clearColor()
+        label.text = "Here I am!"
+        label.font = UIFont(name: "Avernir", size: 30)
+        label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/4 - labelHeight/2)
+        self.view.addSubview(label)
+        
+        let button = UIButton(frame: CGRectMake(self.view.frame.size.width/2, self.view.frame.size.height/2, 200, 100))
+        button.setTitle("Got it", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        button.setTitleColor(UIColor.redColor(), forState: .Highlighted)
+        self.view.addSubview(button)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
